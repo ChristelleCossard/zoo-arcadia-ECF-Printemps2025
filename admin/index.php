0bbf774f6af44b70d02f3d818a7c381268d20f5d
Christelle Cossard <christelle.cossard10@gmail.com>
2024-11-08 14:55:34 +0100

    essai

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@ require_once __DIR__ . "/templates/header.php";
 <div class="px-4 py-5 my-5 text-left">
   <h1 class="display-5 fw-bold text-body-emphasis">Page d'administration</h1>
   <div class="col-lg-12">
-    <p class="lead mb-4">Backoffice du site Garage Parrot pour administrer les Véhicules et services.</p>
+    <p class="lead mb-4">Backoffice du site Zoo Arcadia pour administrer les animaux, leur habitat et les services.</p>
   </div>
 </div>
 
--- admin/services.php
+++ admin/services.php
@@ -3,15 +3,63 @@ require_once __DIR__ . "/../lib/config.php";
 require_once __DIR__ . "/../lib/session.php";
 adminOnly();
 
-
+require_once __DIR__ . "/../lib/pdo.php";
+require_once __DIR__ . "/../lib/service.php";
 require_once __DIR__ . "/templates/header.php"; 
 ?>
 
-<div class="px-4 py-5 my-5 text-left">
-  <h1 class="display-5 fw-bold text-body-emphasis">Page d'administration</h1>
-  <div class="col-lg-12">
-    <p class="lead mb-4">Backoffice du site Garage Parrot pour administrer les Véhicules et services.</p>
-  </div>
+if (isset($_GET['page'])) {
+  $page = (int)$_GET['page'];
+} else {
+  $page = 1;
+}
+$services = getServices($pdo, _ADMIN_ITEM_PER_PAGE, $page);
+
+
+$totalServices = getTotalService($pdo);
+
+$totalPages = ceil($totalService / _ADMIN_ITEM_PER_PAGE);
+
+?>
+
+
+<h1 class="display-5 fw-bold text-body-emphasis">Liste des services</h1>
+<div class="d-flex gap-2 justify-content-left py-5">
+  <a class="btn btn-primary d-inline-flex align-items-left" href="service.php">
+    Ajouter un service
+  </a>
 </div>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titre</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($services as $key => $service) { ?>
+      <tr>
+        <th scope="row"><?= $service["id"]; ?></th>
+        <td><?= $service["nameservice"]; ?></td>
+        <td><a href="service.php?id=<?= $service['id'] ?>">Modifier</a>
+          | <a href="service_delete.php?id=<?= $service['id'] ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce service ?')">Supprimer</a></td>
+      </tr>
+    <?php } ?>
+  </tbody>
+</table>
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <?php if ($totalPages > 1) { ?>
+      <?php for ($i = 1; $i <= $totalPages; $i++) { ?>
+        <li class="page-item">
+          <a class="page-link <?php if ($i == $page) { echo " active";} ?>" href="?page=<?php echo $i; ?>" >
+            <?php echo $i; ?>
+          </a>
+        </li>
+      <?php } ?>
+    <?php } ?>
+  </ul>
+</nav>
 
 <?php require_once('templates/footer.php'); ?>
