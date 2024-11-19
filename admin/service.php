f4c8ecac46298f9d6200a8a0f5b2b9b98c2dc719
Christelle Cossard <christelle.cossard10@gmail.com>
2024-11-19 15:04:27 +0100

    ajout copie a rendre

--- admin/service.php
+++ admin/service.php
@@ -6,33 +6,34 @@ adminOnly();
 require_once __DIR__ . "/../lib/pdo.php";
 require_once __DIR__ . "/../lib/tools.php";
 //require_once __DIR__ . "/../lib/article.php";
-require_once __DIR__ . "/../lib/car.php";
-require_once __DIR__ . "/../lib/category.php";
+//require_once __DIR__ . "/../lib/car.php";
+require_once __DIR__ . "/../lib/service.php";
+//require_once __DIR__ . "/../lib/category.php";
 require_once __DIR__ . "/templates/header.php";
 
 
 $errors = [];
 $messages = [];
-$car = [
+$service = [
     'title' => '',
     'description' => '',
     'category_id' => ''
 ];
 
-$categories = getCategories($pdo);
+//$categories = getCategories($pdo);
 
 if (isset($_GET['id'])) {
     //requête pour récupérer les données de l'article en cas de modification
-    $car = getCarById($pdo, $_GET['id']);
-    if ($car === false) {
+    $service = getServiceById($pdo, $_GET['id']);
+    if ($service === false) {
         $errors[] = "Le vehicule n\'existe pas";
     }
     $pageTitle = "Formulaire modification voiture";
 } else {
-    $pageTitle = "Formulaire ajout voiture";
+    $pageTitle = "Formulaire d'ajout d'un service";
 }
 
-if (isset($_POST['saveCar'])) {
+if (isset($_POST['saveService'])) {
 
     //@todo gérer la gestion des erreurs sur les champs (champ vide etc.)
     
@@ -75,10 +76,10 @@ if (isset($_POST['saveCar'])) {
        les informations dans les champs. C'est utile pas exemple si on upload un mauvais
        fichier et qu'on ne souhaite pas perdre les données qu'on avait saisit.
     */
-    $car = [
+    $service = [
         'title' => $_POST['title'],
         'description' => $_POST['description'],
-        'category_id' => $_POST['category_id'],
+        //'category_id' => $_POST['category_id'],
         'image' => $fileName
     ];
     // Si il n'y a pas d'erreur on peut faire la sauvegarde
@@ -90,13 +91,13 @@ if (isset($_POST['saveCar'])) {
             $id = null;
         }
         // On passe toutes les données à la fonction saveArticle
-        $res = saveCar($pdo, $_POST["title"], $_POST["description"], $fileName, (int)$_POST["category_id"], $id);
+        $res = saveService($pdo, $_POST["title"], $_POST["description"], $fileName, (int)$_POST["category_id"], $id);
 
         if ($res) {
             $messages[] = "Le véhicule a bien été sauvegardé";
             //On vide le tableau article pour avoir les champs de formulaire vides
             if (!isset($_GET["id"])) {
-                $car = [
+                $service = [
                     'title' => '',
                     'description' => '',
                     'category_id' => ''
@@ -121,31 +122,31 @@ if (isset($_POST['saveCar'])) {
         <?= $error; ?>
     </div>
 <?php } ?>
-<?php if ($car !== false) { ?>
+<?php if ($service !== false) { ?>
     <form method="POST" enctype="multipart/form-data">
         <div class="mb-3">
             <label for="title" class="form-label">Titre</label>
-            <input type="text" class="form-control" id="title" name="title" value="<?= $car['title']; ?>">
+            <input type="text" class="form-control" id="title" name="title" value="<?= $service['title']; ?>">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Contenu</label>
-            <textarea class="form-control" id="description" name="description" rows="8"><?= $car['description']; ?></textarea>
+            <textarea class="form-control" id="description" name="description" rows="8"><?= $service['description']; ?></textarea>
         </div>
         <div class="mb-3">
             <label for="category" class="form-label">Catégorie</label>
             <select name="category_id" id="category" class="form-select">
                 <?php foreach ($categories as $category) { ?>
-                    <option value="1" <?php if (isset($car['category_id']) && $car['category_id'] == $category['id']) { ?>selected="selected" <?php }; ?>><?= $category['name'] ?></option>
+                    <option value="1" <?php if (isset($service['category_id']) && $service['category_id'] == $category['id']) { ?>selected="selected" <?php }; ?>><?= $category['name'] ?></option>
                 <?php } ?>
             </select>
         </div>
 
-        <?php if (isset($_GET['id']) && isset($car['image'])) { ?>
+        <?php if (isset($_GET['id']) && isset($service['image'])) { ?>
             <p>
-                <img src="<?= _ARTICLES_IMAGES_FOLDER_ . $car['image'] ?>" alt="<?= $car['title'] ?>" width="100">
+                <img src="<?= _ARTICLES_IMAGES_FOLDER_ . $service['image'] ?>" alt="<?= $car['title'] ?>" width="100">
                 <label for="delete_image">Supprimer l'image</label>
                 <input type="checkbox" name="delete_image" id="delete_image">
-                <input type="hidden" name="image" value="<?= $car['image']; ?>">
+                <input type="hidden" name="image" value="<?= $service['image']; ?>">
 
             </p>
         <?php } ?>
@@ -153,7 +154,7 @@ if (isset($_POST['saveCar'])) {
             <input type="file" name="file" id="file">
         </p>
 
-        <input type="submit" name="saveCar" class="btn btn-primary" value="Enregistrer">
+        <input type="submit" name="saveService" class="btn btn-primary" value="Enregistrer">
 
     </form>
 
